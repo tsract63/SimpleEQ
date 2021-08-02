de5ea6a26883ffac02f7fe208e912e113de5140b
kalukaley <tsract@gmail.com>
2021-08-01 20:01:12 -0400

    Fixed FFT Path position

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -308,8 +308,9 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
 		responsiveCurve.lineTo(responseArea.getX() + i, map(mags[i]));
 	}
 
+	leftChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), responseArea.getY()));
 
-	g.setColour(Colours::blue);
+	g.setColour(Colours::skyblue);
 	g.strokePath(leftChannelFFTPath, PathStrokeType(1.f));
 
 	g.setColour(Colours::orange);
