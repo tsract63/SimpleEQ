ac1174a04cda0d33554d862c66270056678d0465
kalukaley <tsract@gmail.com>
2021-08-03 00:32:11 -0400

    Customized Togglebutton

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin editor.
+	This file contains the basic framework code for a JUCE plugin editor.
 
   ==============================================================================
 */
@@ -13,304 +13,314 @@
 
 enum FFTOrder
 {
-    order2048 = 11,
-    order4096 = 12,
-    order8192 = 13
+	order2048 = 11,
+	order4096 = 12,
+	order8192 = 13
 };
 
 template<typename BlockType>
 struct FFTDataGenerator
 {
-    /**
-     produces the FFT data from an audio buffer.
-     */
-    void produceFFTDataForRendering(const juce::AudioBuffer<float>& audioData, const float negativeInfinity)
-    {
-        const auto fftSize = getFFTSize();
+	/**
+	 produces the FFT data from an audio buffer.
+	 */
+	void produceFFTDataForRendering(const juce::AudioBuffer<float>& audioData, const float negativeInfinity)
+	{
+		const auto fftSize = getFFTSize();
 
-        fftData.assign(fftData.size(), 0);
-        auto* readIndex = audioData.getReadPointer(0);
-        std::copy(readIndex, readIndex + fftSize, fftData.begin());
+		fftData.assign(fftData.size(), 0);
+		auto* readIndex = audioData.getReadPointer(0);
+		std::copy(readIndex, readIndex + fftSize, fftData.begin());
 
-        // first apply a windowing function to our data
-        window->multiplyWithWindowingTable(fftData.data(), fftSize);       // [1]
+		// first apply a windowing function to our data
+		window->multiplyWithWindowingTable(fftData.data(), fftSize);       // [1]
 
-        // then render our FFT data..
-        forwardFFT->performFrequencyOnlyForwardTransform(fftData.data());  // [2]
+		// then render our FFT data..
+		forwardFFT->performFrequencyOnlyForwardTransform(fftData.data());  // [2]
 
-        int numBins = (int)fftSize / 2;
+		int numBins = (int)fftSize / 2;
 
-        //normalize the fft values.
-        for (int i = 0; i < numBins; ++i)
-        {
-            auto v = fftData[i];
-            //            fftData[i] /= (float) numBins;
-            if (!std::isinf(v) && !std::isnan(v))
-            {
-                v /= float(numBins);
-            }
-            else
-            {
-                v = 0.f;
-            }
-            fftData[i] = v;
-        }
+		//normalize the fft values.
+		for (int i = 0; i < numBins; ++i)
+		{
+			auto v = fftData[i];
+			//            fftData[i] /= (float) numBins;
+			if (!std::isinf(v) && !std::isnan(v))
+			{
+				v /= float(numBins);
+			}
+			else
+			{
+				v = 0.f;
+			}
+			fftData[i] = v;
+		}
 
-        //convert them to decibels
-        for (int i = 0; i < numBins; ++i)
-        {
-            fftData[i] = juce::Decibels::gainToDecibels(fftData[i], negativeInfinity);
-        }
+		//convert them to decibels
+		for (int i = 0; i < numBins; ++i)
+		{
+			fftData[i] = juce::Decibels::gainToDecibels(fftData[i], negativeInfinity);
+		}
 
-        fftDataFifo.push(fftData);
-    }
+		fftDataFifo.push(fftData);
+	}
 
-    void changeOrder(FFTOrder newOrder)
-    {
-        //when you change order, recreate the window, forwardFFT, fifo, fftData
-        //also reset the fifoIndex
-        //things that need recreating should be created on the heap via std::make_unique<>
+	void changeOrder(FFTOrder newOrder)
+	{
+		//when you change order, recreate the window, forwardFFT, fifo, fftData
+		//also reset the fifoIndex
+		//things that need recreating should be created on the heap via std::make_unique<>
 
-        order = newOrder;
-        auto fftSize = getFFTSize();
+		order = newOrder;
+		auto fftSize = getFFTSize();
 
-        forwardFFT = std::make_unique<juce::dsp::FFT>(order);
-        window = std::make_unique<juce::dsp::WindowingFunction<float>>(fftSize, juce::dsp::WindowingFunction<float>::blackmanHarris);
+		forwardFFT = std::make_unique<juce::dsp::FFT>(order);
+		window = std::make_unique<juce::dsp::WindowingFunction<float>>(fftSize, juce::dsp::WindowingFunction<float>::blackmanHarris);
 
-        fftData.clear();
-        fftData.resize(fftSize * 2, 0);
+		fftData.clear();
+		fftData.resize(fftSize * 2, 0);
 
-        fftDataFifo.prepare(fftData.size());
-    }
-    //==============================================================================
-    int getFFTSize() const { return 1 << order; }
-    int getNumAvailableFFTDataBlocks() const { return fftDataFifo.getNumAvailableForReading(); }
-    //==============================================================================
-    bool getFFTData(BlockType& fftData) { return fftDataFifo.pull(fftData); }
+		fftDataFifo.prepare(fftData.size());
+	}
+	//==============================================================================
+	int getFFTSize() const { return 1 << order; }
+	int getNumAvailableFFTDataBlocks() const { return fftDataFifo.getNumAvailableForReading(); }
+	//==============================================================================
+	bool getFFTData(BlockType& fftData) { return fftDataFifo.pull(fftData); }
 private:
-    FFTOrder order;
-    BlockType fftData;
-    std::unique_ptr<juce::dsp::FFT> forwardFFT;
-    std::unique_ptr<juce::dsp::WindowingFunction<float>> window;
+	FFTOrder order;
+	BlockType fftData;
+	std::unique_ptr<juce::dsp::FFT> forwardFFT;
+	std::unique_ptr<juce::dsp::WindowingFunction<float>> window;
 
-    Fifo<BlockType> fftDataFifo;
+	Fifo<BlockType> fftDataFifo;
 };
 
 template<typename PathType>
 struct AnalyzerPathGenerator
 {
-    /*
-     converts 'renderData[]' into a juce::Path
-     */
-    void generatePath(const std::vector<float>& renderData,
-        juce::Rectangle<float> fftBounds,
-        int fftSize,
-        float binWidth,
-        float negativeInfinity)
-    {
-        auto top = fftBounds.getY();
-        auto bottom = fftBounds.getHeight();
-        auto width = fftBounds.getWidth();
+	/*
+	 converts 'renderData[]' into a juce::Path
+	 */
+	void generatePath(const std::vector<float>& renderData,
+		juce::Rectangle<float> fftBounds,
+		int fftSize,
+		float binWidth,
+		float negativeInfinity)
+	{
+		auto top = fftBounds.getY();
+		auto bottom = fftBounds.getHeight();
+		auto width = fftBounds.getWidth();
 
-        int numBins = (int)fftSize / 2;
+		int numBins = (int)fftSize / 2;
 
-        PathType p;
-        p.preallocateSpace(3 * (int)fftBounds.getWidth());
+		PathType p;
+		p.preallocateSpace(3 * (int)fftBounds.getWidth());
 
-        auto map = [bottom, top, negativeInfinity](float v)
-        {
-            return juce::jmap(v,
-                negativeInfinity, 0.f,
-                float(bottom + 10), top);
-        };
+		auto map = [bottom, top, negativeInfinity](float v)
+		{
+			return juce::jmap(v,
+				negativeInfinity, 0.f,
+				float(bottom + 10), top);
+		};
 
-        auto y = map(renderData[0]);
+		auto y = map(renderData[0]);
 
-        //        jassert( !std::isnan(y) && !std::isinf(y) );
-        if (std::isnan(y) || std::isinf(y))
-            y = bottom;
+		//        jassert( !std::isnan(y) && !std::isinf(y) );
+		if (std::isnan(y) || std::isinf(y))
+			y = bottom;
 
-        p.startNewSubPath(0, y);
+		p.startNewSubPath(0, y);
 
-        const int pathResolution = 2; //you can draw line-to's every 'pathResolution' pixels.
+		const int pathResolution = 2; //you can draw line-to's every 'pathResolution' pixels.
 
-        for (int binNum = 1; binNum < numBins; binNum += pathResolution)
-        {
-            y = map(renderData[binNum]);
+		for (int binNum = 1; binNum < numBins; binNum += pathResolution)
+		{
+			y = map(renderData[binNum]);
 
-            //            jassert( !std::isnan(y) && !std::isinf(y) );
+			//            jassert( !std::isnan(y) && !std::isinf(y) );
 
-            if (!std::isnan(y) && !std::isinf(y))
-            {
-                auto binFreq = binNum * binWidth;
-                auto normalizedBinX = juce::mapFromLog10(binFreq, 20.f, 20000.f);
-                int binX = std::floor(normalizedBinX * width);
-                p.lineTo(binX, y);
-            }
-        }
+			if (!std::isnan(y) && !std::isinf(y))
+			{
+				auto binFreq = binNum * binWidth;
+				auto normalizedBinX = juce::mapFromLog10(binFreq, 20.f, 20000.f);
+				int binX = std::floor(normalizedBinX * width);
+				p.lineTo(binX, y);
+			}
+		}
 
-        pathFifo.push(p);
-    }
+		pathFifo.push(p);
+	}
 
-    int getNumPathsAvailable() const
-    {
-        return pathFifo.getNumAvailableForReading();
-    }
+	int getNumPathsAvailable() const
+	{
+		return pathFifo.getNumAvailableForReading();
+	}
 
-    bool getPath(PathType& path)
-    {
-        return pathFifo.pull(path);
-    }
+	bool getPath(PathType& path)
+	{
+		return pathFifo.pull(path);
+	}
 private:
-    Fifo<PathType> pathFifo;
+	Fifo<PathType> pathFifo;
 };
 
 
-struct LookAndFeel :  juce::LookAndFeel_V4
+struct LookAndFeel : juce::LookAndFeel_V4
 {
-    void drawRotarySlider(juce::Graphics&, int x, int y, int width, int height,
-        float sliderPosProportional, float rotaryStartAngle,
-        float rotaryEndAngle, juce::Slider&) override;
+	void drawRotarySlider(juce::Graphics&, int x, int y, int width, int height,
+		float sliderPosProportional, float rotaryStartAngle,
+		float rotaryEndAngle, juce::Slider&) override;
+
+
+
+	void drawToggleButton(juce::Graphics&,
+		juce::ToggleButton&,
+		bool 	shouldDrawButtonAsHighlighted,
+		bool 	shouldDrawButtonAsDown
+	)override;
 };
 
 struct RotarySliderWithLabels : public juce::Slider
 {
-    RotarySliderWithLabels( juce::RangedAudioParameter& rap, const juce::String& unitSuffix) : 
-        juce::Slider( juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox),
-        param(&rap),
-        suffix(unitSuffix)
-    {
-        setLookAndFeel(&lnf);
-    }
+	RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
+		juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox),
+		param(&rap),
+		suffix(unitSuffix)
+	{
+		setLookAndFeel(&lnf);
+	}
 
-    ~RotarySliderWithLabels()
-    {
-        setLookAndFeel(nullptr);
-    }
+	~RotarySliderWithLabels()
+	{
+		setLookAndFeel(nullptr);
+	}
 
-    struct LabelPos
-    {
-        float pos;
-        juce::String label;
-    };
+	struct LabelPos
+	{
+		float pos;
+		juce::String label;
+	};
 
-    juce::Array<LabelPos> labels;
+	juce::Array<LabelPos> labels;
 
-    void paint(juce::Graphics&) override;
-    juce::Rectangle<int> getSliderBounds() const;
-    int getTextHeight() const { return 14; }
-    juce::String getDisplayString() const;
+	void paint(juce::Graphics&) override;
+	juce::Rectangle<int> getSliderBounds() const;
+	int getTextHeight() const { return 14; }
+	juce::String getDisplayString() const;
 private:
-    LookAndFeel lnf;
-    juce::RangedAudioParameter* param;
-    juce::String suffix;
+	LookAndFeel lnf;
+	juce::RangedAudioParameter* param;
+	juce::String suffix;
 };
 
 struct PathProducer
 {
-    PathProducer(SingleChannelSampleFifo<SimpleEQAudioProcessor::BlockType>& scsf) :
-        leftChannelFifo(&scsf)
-    {
+	PathProducer(SingleChannelSampleFifo<SimpleEQAudioProcessor::BlockType>& scsf) :
+		leftChannelFifo(&scsf)
+	{
 
-        leftChannelFFTDataGenerator.changeOrder(FFTOrder::order2048);
-        monoBuffer.setSize(1, leftChannelFFTDataGenerator.getFFTSize());
-    }
-    void process(juce::Rectangle<float> fftBounds, double sampleRate);
-    juce::Path getPath() { return leftChannelFFTPath; }
+		leftChannelFFTDataGenerator.changeOrder(FFTOrder::order2048);
+		monoBuffer.setSize(1, leftChannelFFTDataGenerator.getFFTSize());
+	}
+	void process(juce::Rectangle<float> fftBounds, double sampleRate);
+	juce::Path getPath() { return leftChannelFFTPath; }
 private:
-    SingleChannelSampleFifo<SimpleEQAudioProcessor::BlockType>* leftChannelFifo;
+	SingleChannelSampleFifo<SimpleEQAudioProcessor::BlockType>* leftChannelFifo;
 
-    juce::AudioBuffer<float>monoBuffer;
+	juce::AudioBuffer<float>monoBuffer;
 
-    FFTDataGenerator< std::vector<float> > leftChannelFFTDataGenerator;
+	FFTDataGenerator< std::vector<float> > leftChannelFFTDataGenerator;
 
-    AnalyzerPathGenerator<juce::Path> pathProducer;
+	AnalyzerPathGenerator<juce::Path> pathProducer;
 
-    juce::Path leftChannelFFTPath;
+	juce::Path leftChannelFFTPath;
 };
 
 struct ResponseCurveComponent : public juce::Component,
-    public juce::AudioProcessorParameter::Listener,
-    public juce::Timer
+	public juce::AudioProcessorParameter::Listener,
+	public juce::Timer
 {
-    ResponseCurveComponent(SimpleEQAudioProcessor&);
-    ~ResponseCurveComponent() override;
+	ResponseCurveComponent(SimpleEQAudioProcessor&);
+	~ResponseCurveComponent() override;
 
-    void parameterValueChanged(int parameterIndex, float newValue) override;    
-    void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override {}
-    void paint(juce::Graphics&)override;
-    void resized() override;
+	void parameterValueChanged(int parameterIndex, float newValue) override;
+	void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override {}
+	void paint(juce::Graphics&)override;
+	void resized() override;
 
-    void timerCallback() override;
+	void timerCallback() override;
 private:
-    SimpleEQAudioProcessor& audioProcessor;
-    juce::Atomic<bool> parametersChanged = { false };
+	SimpleEQAudioProcessor& audioProcessor;
+	juce::Atomic<bool> parametersChanged = { false };
 
-    MonoChain monoChain;
-    void updateChain();
+	MonoChain monoChain;
+	void updateChain();
 
-    juce::Image background;
+	juce::Image background;
 
-    juce::Rectangle<int> getRenderArea();
-    juce::Rectangle<int> getAnalysisArea();
+	juce::Rectangle<int> getRenderArea();
+	juce::Rectangle<int> getAnalysisArea();
 
-    PathProducer leftPathProducer, rightPathProducer;
+	PathProducer leftPathProducer, rightPathProducer;
 
 
 };
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
-    SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
-    ~SimpleEQAudioProcessorEditor() override;
+	SimpleEQAudioProcessorEditor(SimpleEQAudioProcessor&);
+	~SimpleEQAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+	//==============================================================================
+	void paint(juce::Graphics&) override;
+	void resized() override;
 
-   
+
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    SimpleEQAudioProcessor& audioProcessor;
+	// This reference is provided as a quick way for your editor to
+	// access the processor object that created it.
+	SimpleEQAudioProcessor& audioProcessor;
 
 
-    RotarySliderWithLabels
-        peakFreqSlider,
-        peakGainSlider,
-        peakQualitySlider,
-        lowCutFreqSlider,
-        highCutFreqSlider,
-        lowCutSlopeSlider,
-        highCutSlopeSlider;
+	RotarySliderWithLabels
+		peakFreqSlider,
+		peakGainSlider,
+		peakQualitySlider,
+		lowCutFreqSlider,
+		highCutFreqSlider,
+		lowCutSlopeSlider,
+		highCutSlopeSlider;
 
-    ResponseCurveComponent responseCurveComponent;
+	ResponseCurveComponent responseCurveComponent;
 
-    using APVTS = juce::AudioProcessorValueTreeState;
-    using Attachment = APVTS::SliderAttachment;
+	using APVTS = juce::AudioProcessorValueTreeState;
+	using Attachment = APVTS::SliderAttachment;
 
-    Attachment peakFreqSliderAttachment,
-        peakGainSliderAttachment,
-        peakQualitySliderAttachment,
-        lowCutFreqSliderAttachment,
-        highCutFreqSliderAttachment,
-        lowCutSlopeSliderAttachment,
-        highCutSlopeSliderAttachment;
+	Attachment peakFreqSliderAttachment,
+		peakGainSliderAttachment,
+		peakQualitySliderAttachment,
+		lowCutFreqSliderAttachment,
+		highCutFreqSliderAttachment,
+		lowCutSlopeSliderAttachment,
+		highCutSlopeSliderAttachment;
 
-    juce::ToggleButton lowcutBypassButton, peakBypassButton, highcutBypassButton, analyzerEnabledButton;
+	juce::ToggleButton lowcutBypassButton, peakBypassButton, highcutBypassButton, analyzerEnabledButton;
 
-    using ButtonAttachment = APVTS::ButtonAttachment;
-    ButtonAttachment lowcutBypassButtonAttachment,
-        peakBypassButtonAttachment,
-        highcutBypassButtonAttachment,
-        analyzerEnabledButtonAttachment;
+	using ButtonAttachment = APVTS::ButtonAttachment;
+	ButtonAttachment lowcutBypassButtonAttachment,
+		peakBypassButtonAttachment,
+		highcutBypassButtonAttachment,
+		analyzerEnabledButtonAttachment;
 
-    std::vector<juce::Component*> getComps();
+	std::vector<juce::Component*> getComps();
+
+	LookAndFeel lnf;
 
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SimpleEQAudioProcessorEditor)
 };
